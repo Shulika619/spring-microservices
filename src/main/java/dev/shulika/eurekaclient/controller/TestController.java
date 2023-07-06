f7c69823e3fd86698db3481c7597c9b944fa1f62
Sergey Shulika <developer.shulika@gmail.com>
2023-07-06 19:30:41 +0300

    added spring cloud config

--- src/main/java/dev/shulika/eurekaclient/controller/TestController.java
+++ src/main/java/dev/shulika/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package dev.shulika.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instanceId}")
+    private String id;
+
+    @Value("${tmpVar}")
+    private String tmpVar;
+
     @GetMapping("/test")
     public String test(){
-        return "test";
+        return "test - Client1 :: id - " + id + " tempVar = " + tmpVar;
     }
 }
